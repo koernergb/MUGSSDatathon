7e84c69cc355ab74220f6fc53ebab26e795ea721
koernergb <koernerg@umich.edu>
2024-03-09 10:01:01 -0500

    added imports

---
+++ data_analysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import scipy as sp
+import numpy as np
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.datasets import load_iris
+
+# Load data
+
+# Calculate metrics
+
+# Visualize results
